51be093444ac7e647f88b89dde2833a4f161f12e
user <user@user.com>
2026-02-22 17:07:02 +0700

    Change welcome text

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def hello():
     db_version = cur.fetchone()
     cur.close()
     conn.close()
-    return f"Привет! Я работаю из контейнера. Это визит №{count}.  База говорит: {db_version}"
+    return f"Привет! Я работаю из Облака Яндекса через GitHub Actions! Это визит №{count}.  База говорит: {db_version}"
 
 if __name__ == '__main__':
     init_db()
